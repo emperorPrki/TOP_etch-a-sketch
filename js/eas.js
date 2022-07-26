4c374addf3f5604941154a7a60d4d3eae9d2bff3
emperorPrki <chaboggg@gmail.com>
2022-07-26 16:58:36 +0200

    Add functions necessary for the website to work
    - Add resetGrid() which removes all existing child nodes of #eas-container
    - Add makeGrid() which creates grid according to pressed button
    - Add makeActive() which applies .active and changes .easBox bg-clr to black
        after hovering over the cell, is default once page is loaded
    - Add logic to create a grid appropriate to button pressed

---
+++ js/eas.js
@@ -0,0 +1,71 @@
+makeGrid100();
+makeActive();
+
+const chooseGridSize = document.querySelectorAll("div#sizeButtons button")
+chooseGridSize.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.id === "grid100") {
+            resetGrid();
+            makeGrid100();
+            makeActive();
+        } else if (button.id === "grid400"){
+            resetGrid();
+            makeGrid400();
+            makeActive();
+        } else if (button.id === "grid900"){
+            resetGrid();
+            makeGrid900();
+            makeActive();
+        }
+    })
+})
+
+function makeActive(){
+    const gridCellArray = document.querySelectorAll(".easBox")
+    gridCellArray.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+        div.setAttribute("class", "active");
+        })
+    })
+}
+
+function makeGrid100(){
+    const selectEasContainer = document.querySelector("#eas-container");
+    selectEasContainer.setAttribute("style", "grid-template-columns: repeat(10, auto)");
+
+    for(let i = 0; i < 100; i++){
+        const createGridCell = document.createElement("div");
+        createGridCell.setAttribute("class", "easBox");
+        selectEasContainer.appendChild(createGridCell);
+    }
+}
+
+function makeGrid400(){
+    const selectEasContainer = document.querySelector("#eas-container");
+    selectEasContainer.setAttribute("style", "grid-template-columns: repeat(20, auto)");
+
+    for(let i = 0; i < 400; i++){
+        const createGridCell = document.createElement("div");
+        createGridCell.setAttribute("class", "easBox");
+        selectEasContainer.appendChild(createGridCell);
+    }
+}
+
+function makeGrid900(){
+    const selectEasContainer = document.querySelector("#eas-container");
+    selectEasContainer.setAttribute("style", "grid-template-columns: repeat(30, auto)");
+
+    for(let i = 0; i < 900; i++){
+        const createGridCell = document.createElement("div");
+        createGridCell.setAttribute("class", "easBox");
+        selectEasContainer.appendChild(createGridCell);
+    }
+}
+
+function resetGrid(){
+    const selectEasContainer = document.querySelector("#eas-container");
+
+    while(selectEasContainer.hasChildNodes()){
+        selectEasContainer.removeChild(selectEasContainer.firstChild);
+    }
+}
