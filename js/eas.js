4fe0800741bd8f498112f924fe66dbf0f0f74be8
emperorPrki <chaboggg@gmail.com>
2022-07-29 17:23:21 +0200

    Fix gradual() to not paint cells rainbow

--- js/eas.js
+++ js/eas.js
@@ -1,33 +1,53 @@
 makeGrid100();
 makeActive();
 
-const chooseGridSize = document.querySelectorAll("div#sizeButtons button")
+const chooseColorWay = document.querySelectorAll("div#controlButtons button");
+chooseColorWay.forEach((button) => {
+    button.addEventListener("click", () => {
+        switch(button.id){
+            case "black":
+
+                makeActive();
+                console.log("black")
+                break;
+            
+            case "gradual":
+
+                makeGradual();
+                console.log("gradual")
+                break;
+
+            case "rainbow":
+
+                makeRainbow();
+                console.log("rainbow")
+                break;
+        }
+    })
+}) 
+
+    const chooseGridSize = document.querySelectorAll("div#sizeButtons button");
 chooseGridSize.forEach((button) => {
     button.addEventListener("click", () => {
         if (button.id === "grid100") {
             resetGrid();
             makeGrid100();
-            makeRainbow();
         } else if (button.id === "grid400"){
             resetGrid();
             makeGrid400();
-            makeActive();
         } else if (button.id === "grid900"){
             resetGrid();
             makeGrid900();
-            makeActive();
         }
     })
 })
-
-
 
 function makeRainbow(){
 
     const gridCellArray = document.querySelectorAll(".easBox");
     gridCellArray.forEach((div) => {
         div.addEventListener("mouseover", () => {
-            
+
             const rgb1 = Math.floor(Math.random()*256);
             const rgb2 = Math.floor(Math.random()*256);
             const rgb3 = Math.floor(Math.random()*256);
@@ -40,31 +60,36 @@ function makeRainbow(){
                 arrayValues[i] = rgbArray[i];
             }
             div.style.backgroundColor = "rgb(" + arrayValues.join(",") + ")";
-        })
+        });
     })
 }
 
 function makeGradual(){
-    const gridCellArray = document.querySelectorAll(".easBox");
-    gridCellArray.forEach((div) => {
-        div.addEventListener("mouseover", () => {
-            const colorValue = getComputedStyle(div).getPropertyValue("background-color");
-            const arrayValues = colorValue.match(/[\d]+/g)
+    
+        const gridCellArray = document.querySelectorAll(".easBox");
+        gridCellArray.forEach((div) => {
 
-            for (let i = 0; i < arrayValues.length; i++){
-                arrayValues[i] = arrayValues[i] - 51;
-            }
-            div.style.backgroundColor = "rgb(" + arrayValues.join(",") + ")";
-        })
-    })
+            const colorValue = getComputedStyle(div).getPropertyValue("background-color");
+            const arrayValues = colorValue.match(/[\d.]+/g);
+            
+            div.addEventListener("mouseover", () => { 
+
+                for (let i = 0; i < arrayValues.length; i++){
+                    arrayValues[i] = arrayValues[i] - 51;
+                }
+                div.style.backgroundColor = "rgb(" + arrayValues.join(",") + ")";
+            }) 
+        });   
 }
 
 function makeActive(){
+
     const gridCellArray = document.querySelectorAll(".easBox");
+    const rgbBlack = [0, 0, 0];
     gridCellArray.forEach((div) => {
         div.addEventListener("mouseover", () => {
-        div.classList.add("active");
-        })
+        div.style.backgroundColor = "rgb(" + rgbBlack.join(",") + ")"
+        });
     })
 }
 
